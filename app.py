7339a17ea65550edc0342c0f850308ee98172f18
vikas981 <vs98990@gmail.com>
2020-04-27 18:02:11 +0530

    Changes

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template, url_for, request, redirect, flash, session, send_from_directory, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import os
 import json
 
 # Init app
 app = Flask(__name__)
 # Database
-basedir = os.path.abspath(os.path.dirname(__file__))
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
 
 app.secret_key = 'vikash'
